9d7898ea07248ba34b678be63d7547a116f46a5d
zhushangs <zhushangs@gmail.com>
2021-04-13 17:33:16 -0400

    added promoise

---
+++ Promise/Promise/JS/callAPIs.js
@@ -0,0 +1,47 @@
+﻿function getComments() {
+    fetch("https://jsonplaceholder.typicode.com/comments").then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        let tbody = document.querySelector("tbody");
+        tbody.innerHTML = ""
+        let length = data.length;
+        for (let i = 0; i < length; i++) {
+            let tr = "<tr> <td>" + data[i].id + "</td> <td>" + data[i].name + "</td> <td>" + data[i].email + "</td> <td>" + data[i].body + "</td> </tr>"
+            tbody.innerHTML += tr;
+        }
+    }).catch(function (e) {
+        console.log(e)
+    })
+}
+
+function getTodos() {
+    fetch("https://jsonplaceholder.typicode.com/todos").then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        let tbody = document.querySelector("tbody");
+        tbody.innerHTML = ""
+        let length = data.length;
+        for (let i = 0; i < length; i++) {
+            let tr = "<tr> <td>" + data[i].id + "</td> <td>" + data[i].title + "</td> <td>" + data[i].completed + "</td> </tr>"
+            tbody.innerHTML += tr;
+        }
+    }).catch(function (e) {
+        console.log(e)
+    })
+}
+
+function getPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts").then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        let tbody = document.querySelector("tbody");
+        tbody.innerHTML = ""
+        let length = data.length;
+        for (let i = 0; i < length; i++) {
+            let tr = "<tr> <td>" + data[i].id + "</td> <td>" + data[i].title + "</td> <td>" + data[i].body + "</td> </tr>"
+            tbody.innerHTML += tr;
+        }
+    }).catch(function (e) {
+        console.log(e)
+    })
+}
